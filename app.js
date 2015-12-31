ffeff3858336f4c62a33f62677217d0b3b49ff79
Yilin Xu <yx296@nyu.edu>
2015-12-30 23:25:46 -0800

    Implement stateAutocomplete with ., improve variable names

--- app.js
+++ app.js
@@ -1,22 +1,19 @@
-angular.module('states', [])
-  .controller('stateAutocomplete', function($scope) {
+angular.module('stateAutocomplete', [])
+  .controller('stateAutocompleteCtrl', function($scope) {
     
-
-    $scope.autoCompleteState = '';
+    $scope.stateInput = '';
 
     $scope.states = ["Alabama", "Alaska", "American Samoa", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "District Of Columbia", "Federated States Of Micronesia", "Florida", "Georgia", "Guam", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Marshall Islands", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Northern Mariana Islands", "Ohio", "Oklahoma", "Oregon", "Palau", "Pennsylvania", "Puerto Rico", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virgin Islands", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"];
 
     $scope.filteredStates = [];
 
-    $scope.filterStates = function() {
-
+    $scope.$watch('stateInput', function() {
       $scope.filteredStates = $scope.states.filter(function(state) {
         state = state.toLowerCase();
-        var lowerCaseInput = $scope.autoCompleteState.toLowerCase();
+        var lowerCaseInput = $scope.stateInput.toLowerCase();
         return state.indexOf(lowerCaseInput) > -1;
       })
-    }
-    
+    })
   }) 
 
     
